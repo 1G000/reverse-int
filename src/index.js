15830e20ac28163667b4d80bac9213c3bf354111
User <gorbatovskiy.igor@gmail.com>
2023-07-31 15:35:46 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n).toString().split("");
+    newArr = [];
+    n.forEach((el) => {
+        newArr.unshift(el);
+    });
+    return +newArr.join("");
+};
